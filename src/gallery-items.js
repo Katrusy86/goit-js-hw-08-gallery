67923d8496b042661d5ba9125e3a2bc4072766bd
Katrusy86 <katrin.myronova@gmail.com>
2020-03-29 13:51:00 +0300

    Added a new hw-08-gallery

---
+++ src/gallery-items.js
@@ -0,0 +1,176 @@
+export default [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/view-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/01/16/10/eye-1173863__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/01/16/10/eye-1173863_960_720.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/03/27/17/14/eyes-1283163__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/03/27/17/14/eyes-1283163_960_720.jpg',
+    description: 'Aerial Beach View',
+  },
+
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/31/graffiti-wall-1209761__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/31/graffiti-wall-1209761_960_720.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_960_720.jpg',
+    description: 'Container Haulage Freight',
+  },
+
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_960_720.jpg',
+    description: 'Aerial Beach View',
+  },
+
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782_960_720.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/36/canal-1209808__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/02/19/11/36/canal-1209808_960_720.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_960_720.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/02/27/16/10/spring-276014__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/02/27/16/10/spring-276014_960_720.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2012/08/06/00/53/bridge-53769__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2012/08/06/00/53/bridge-53769_960_720.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+   
+];
+
---
+++ src/task-8.js
@@ -0,0 +1,97 @@
+import galleryItems from './gallery-items.js';
+('use strict')
+
+const refs = {
+    galleryItem: document.querySelector('.js-gallery'),
+    modalWindow: document.querySelector('.js-lightbox'),
+    lightboxImg: document.querySelector('.lightbox__image'),
+    closedButton: document.querySelector('button[data-action="close-lightbox"]'),
+    parentImg: null
+}
+
+const fragment = document.createDocumentFragment();
+galleryItems.forEach(image => {
+    refs.galleryItem.insertAdjacentHTML('beforeend', 
+    `<li class="gallery__item">
+    <a class="gallery__link" href=${image.original}>
+        <img class="gallery__image" src=${image.preview} data-source=${image.original} alt="${image.description}" />
+    </a>
+    </li>`)
+})
+refs.galleryItem.appendChild(fragment);
+
+refs.galleryItem.addEventListener('click', openModal);
+
+function openModal(e){
+    e.preventDefault();
+    if (e.target.nodeName === 'IMG') {
+        refs.parentImg = e.target.closest('li');
+        refs.modalWindow.classList.add('is-open') ;   
+        refs.lightboxImg.setAttribute('src', e.target.dataset.source)
+        
+    }
+    window.addEventListener('keydown',keyPress);
+    
+}
+
+refs.closedButton.addEventListener('click', closedBtn)
+
+function closedBtn(e){
+    if(e.target.nodeName !== 'IMG'){
+        refs.modalWindow.classList.remove('is-open') 
+        refs.lightboxImg.src = '';            
+    } 
+    window.removeEventListener('keydown',keyPress);
+}
+refs.modalWindow.addEventListener('click', closedBtn) //Закрытие модального окна по клику на div.lightbox__overlay.
+
+
+function keyPress(evn){
+    if (evn.code ==='Escape') {
+    closedBtn(evn)
+    }
+        else if (evn.code === 'ArrowRight'){
+            const nextLi = refs.parentImg.nextSibling 
+            refs.parentImg = nextLi;
+            const newImg = nextLi.querySelector('img');
+            const newDataSourse = newImg.dataset.source;
+            refs.lightboxImg.setAttribute('src', newDataSourse)
+            console.log(newDataSourse)
+    }  
+    else if (evn.code === 'ArrowLeft'){
+        const previousLi = refs.parentImg.previousSibling 
+        refs.parentImg = previousLi;
+        const newImg =  previousLi.querySelector('img');
+        const newDataSourse = newImg.dataset.source;
+        refs.lightboxImg.setAttribute('src', newDataSourse)
+        console.log(newDataSourse)
+}   
+    return
+}
+
+
+// ====== Lazyload for gallery======
+
+const lazyLoad = target =>{
+    const options = {};
+
+    const io = new IntersectionObserver((entries, observer)=>{
+        entries.forEach(entry=>{
+            console.log(entry.target)
+            if(entry.isIntersecting){
+                const img = entry.target;
+                const imageUrl = img.dataset.source;
+
+                img.setAttribute('src', imageUrl);
+                observer.disconnect()
+            }
+        })
+    },options)
+
+    io.observe(target);
+};
+const items = refs.galleryItem.querySelectorAll('li')
+
+items.forEach(li =>{
+    lazyLoad(li);
+});
